6c491a69e8f35fb14c7b5faa9c7e2405b0d32468
evanejk <evanejk@gmail.com>
2025-03-30 09:44:36 -0400

    do the jar

--- src/main/java/com/happynicetime/blackyellow/App.java
+++ src/main/java/com/happynicetime/blackyellow/App.java
@@ -33,7 +33,7 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         launch();
     }
 
