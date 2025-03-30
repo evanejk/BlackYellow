8c428b0c01f5a4647017d6093e0b0b51521e332f
evanejk <evanejk@gmail.com>
2025-03-30 16:37:52 -0400

    add color options blue white white blue

--- src/main/java/com/happynicetime/blackyellow/PrimaryController.java
+++ src/main/java/com/happynicetime/blackyellow/PrimaryController.java
@@ -23,7 +23,7 @@ import javafx.scene.control.Slider;
 public class PrimaryController {
     @FXML
     private ChoiceBox<String> choice;
-    private final String[] choices = {"black yellow","yellow black","red blue","blue red","green black","black green","purple yellow","yellow purple"};
+    private final String[] choices = {"black yellow","yellow black","red blue","blue red","green black","black green","purple yellow","yellow purple","blue white","white blue"};
     @FXML
     public void initialize(){
         choice.getItems().addAll(choices);
@@ -205,8 +205,40 @@ public class PrimaryController {
                     double avg = (red + green + blue) / 3d;
                     double inverse = 1d - avg;
                     double newRed = ((weight1 * 1) + (weight1 * inverse) + (weight2 * red))/(weight1+weight1+weight2);
-                    double newGreen = ((weight1 * inverse) + (weight2 * green))/100d;;
+                    double newGreen = ((weight1 * inverse) + (weight2 * green))/100d;
                     double newBlue =  ((weight1 * avg) + (weight2 * blue))/100d;
+                    Color color2 = new Color(newRed,newGreen,newBlue,1f);
+                    pixelWriter.setColor(x, y, color2);
+                }
+            }
+        }else if(choice.getValue().equals("blue white")){
+            for(int x = 0;x < pixelsWidth;x++){
+                for(int y = 0;y < pixelsHeight;y++){
+                    Color color = pixelReader.getColor(x, y);
+                    double red = color.getRed();
+                    double green = color.getGreen();
+                    double blue = color.getBlue();
+                    double avg = (red + green + blue) / 3d;
+                    double inverse = 1d - avg;
+                    double newRed = ((weight1 * avg) + (weight2 * red))/(weight1+weight2);
+                    double newGreen = ((weight1 * avg) + (weight2 * green))/(weight1+weight2);
+                    double newBlue =  ((weight1 * 1) + (weight1 * avg) + (weight2 * blue))/(weight1+weight1+weight2);
+                    Color color2 = new Color(newRed,newGreen,newBlue,1f);
+                    pixelWriter.setColor(x, y, color2);
+                }
+            }
+        }else if(choice.getValue().equals("white blue")){
+            for(int x = 0;x < pixelsWidth;x++){
+                for(int y = 0;y < pixelsHeight;y++){
+                    Color color = pixelReader.getColor(x, y);
+                    double red = color.getRed();
+                    double green = color.getGreen();
+                    double blue = color.getBlue();
+                    double avg = (red + green + blue) / 3d;
+                    double inverse = 1d - avg;
+                    double newRed = ((weight1 * inverse) + (weight2 * red))/(weight1+weight2);
+                    double newGreen = ((weight1 * inverse) + (weight2 * green))/(weight1+weight2);
+                    double newBlue =  ((weight1 * 1) + (weight1 * inverse) + (weight2 * blue))/(weight1+weight1+weight2);
                     Color color2 = new Color(newRed,newGreen,newBlue,1f);
                     pixelWriter.setColor(x, y, color2);
                 }
